13dda2aad2bb4ba7734f7a57d4709b372870207d
panyang <panyang@yuzhenkeji.com>
2017-04-24 21:18:21 +0800

    add version one for train word2vec model by gensim on wikipedia

---
+++ v1/train_word2vec_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import os.path
+import sys
+import multiprocessing
+
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+
+if __name__ == '__main__':
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+
+    # check and process input arguments
+    if len(sys.argv) < 4:
+        print(globals()['__doc__'] % locals())
+        sys.exit(1)
+    inp, outp1, outp2 = sys.argv[1:4]
+
+    model = Word2Vec(LineSentence(inp), size=400, window=5, min_count=5,
+                     workers=multiprocessing.cpu_count())
+
+    # trim unneeded model memory = use(much) less RAM
+    # model.init_sims(replace=True)
+    model.save(outp1)
+    model.save_word2vec_format(outp2, binary=False)
