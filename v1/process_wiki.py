11aab9845c4c6455bf04bc9baf98f17fc5c15bf7
panyang <panyang@yuzhenkeji.com>
2017-05-01 22:25:38 +0800

    modified process_wiki.py to compatible with python2 and python3

--- v1/process_wiki.py
+++ v1/process_wiki.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Author: Pan Yang (panyangnlp@gmail.com)
+# Copyrigh 2017
+
+from __future__ import print_function
 
 import logging
 import os.path
+import six
 import sys
 
 from gensim.corpora import WikiCorpus
@@ -16,8 +21,8 @@ if __name__ == '__main__':
     logger.info("running %s" % ' '.join(sys.argv))
 
     # check and process input arguments
-    if len(sys.argv) < 3:
-        print(globals()['__doc__'] % locals())
+    if len(sys.argv) != 3:
+        print("Using: python process_wiki.py enwiki.xxx.xml.bz2 wiki.en.text")
         sys.exit(1)
     inp, outp = sys.argv[1:3]
     space = " "
@@ -26,7 +31,13 @@ if __name__ == '__main__':
     output = open(outp, 'w')
     wiki = WikiCorpus(inp, lemmatize=False, dictionary={})
     for text in wiki.get_texts():
-        output.write(space.join(text) + "\n")
+        if six.PY3:
+            output.write(b' '.join(text).decode('utf-8') + '\n')
+        #   ###another method###
+        #    output.write(
+        #            space.join(map(lambda x:x.decode("utf-8"), text)) + '\n')
+        else:
+            output.write(space.join(text) + "\n")
         i = i + 1
         if (i % 10000 == 0):
             logger.info("Saved " + str(i) + " articles")
